9da24cc22b171227986cb3a6906b6779687dbc75
Leandro Henrique <leandrohamorim@outlook.com>
2021-05-14 03:39:18 -0300

    Array List

---
+++ src/arrayList/Application.java
@@ -0,0 +1,64 @@
+package arrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Application {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		var sc = new Scanner(System.in);
+
+		List<Employee> list = new ArrayList<>();
+
+		System.out.print("How many employees will be registered? ");
+		final var n = sc.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+			System.out.println();
+			System.out.println("Employee #" + i + ": ");
+
+			System.out.print("Id: ");
+			var id = sc.nextInt();
+			while (hasId(list, id)) {
+				System.out.print("Id already taken. Try again: ");
+				id = sc.nextInt();
+			}
+
+			System.out.print("Name: ");
+			sc.nextLine();
+			var name = sc.nextLine();
+			System.out.print("Salary: ");
+			var salary = sc.nextDouble();
+			list.add(new Employee(id, name, salary));
+		}
+
+		System.out.println();
+		System.out.print("Enter the employee id that will have salary increase: ");
+		var id = sc.nextInt();
+		var emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+		if (emp == null) {
+			System.out.println("This id does not exist!");
+		} else {
+			System.out.print("Enter the percentage: ");
+			var percentage = sc.nextDouble();
+			emp.salaryIncrease(percentage);
+		}
+
+		System.out.println();
+		System.out.println("List of employees:");
+		for (Employee obj : list) {
+			System.out.println(obj);
+		}
+
+		sc.close();
+	}
+
+	public static boolean hasId(List<Employee> list, int id) {
+		var emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+		return emp != null;
+	}
+}
