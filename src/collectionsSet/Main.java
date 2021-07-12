f4ee982f5b4a5cd79c09bc07cbbeea46df354649
Leandro Henrique <leandrohamorim@outlook.com>
2021-07-12 16:07:16 -0300

    Collections Set

---
+++ src/collectionsSet/Main.java
@@ -0,0 +1,47 @@
+package collectionsSet;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		var sc = new Scanner(System.in);
+		Set<Integer> studentsCourseA = new HashSet<>();
+		Set<Integer> studentsCourseB = new HashSet<>();
+		Set<Integer> studentsCourseC = new HashSet<>();
+
+		System.out.println("How many students for course A? ");
+		int amountStudents = sc.nextInt();
+		for (int i = 0; i < amountStudents; i++) {
+			int studentNumber = sc.nextInt();
+			studentsCourseA.add(studentNumber);
+		}
+
+		System.out.println("How many students for course B? ");
+		amountStudents = sc.nextInt();
+		for (int i = 0; i < amountStudents; i++) {
+			int studentNumber = sc.nextInt();
+			studentsCourseB.add(studentNumber);
+		}
+
+		System.out.println("How many students for course C? ");
+		amountStudents = sc.nextInt();
+		for (int i = 0; i < amountStudents; i++) {
+			int studentNumber = sc.nextInt();
+			studentsCourseC.add(studentNumber);
+		}
+		
+		Set<Integer> total = new HashSet<>(studentsCourseA);
+		total.addAll(studentsCourseB);
+		total.addAll(studentsCourseC);
+
+		System.out.println("Total students: " + total.size());
+		
+
+		sc.close();
+	}
+
+}
